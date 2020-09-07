e2bdefda55a35ef6574ec891e7616aa8b6cbc004
PlutonLib <45429108+PlutonLib@users.noreply.github.com>
2020-09-07 22:28:34 +0300

    Add files via upload

---
+++ CTXUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Developer/Coder;W4H9LT")]
+[assembly: AssemblyDescription("Developer/Coder;W4H9LT")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Developer/Coder;W4H9LT")]
+[assembly: AssemblyProduct("Developer/Coder;W4H9LT")]
+[assembly: AssemblyCopyright("Developer/Coder;W4H9LT")]
+[assembly: AssemblyTrademark("Developer/Coder;W4H9LT")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cbe78a27-6c67-44fc-8444-7dcd49b5a815")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
